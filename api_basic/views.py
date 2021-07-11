314996890369943ecf6020096db1dfada09e5da4
brayan <brayanperezbuitrago@gmail.com>
2021-07-11 10:19:19 -0500

    Carga del proyecto al repositorio

---
+++ api_basic/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from .models import Article
+from .serializers import ArticleSerializer
+
+
+def article_list(request):
+	if request.method == 'GET':
+		articles = Article.objects.all()
+		serializer = ArticleSerializer(articles, many=True)
+		return JsonResponse(serializer.data, safe=False)
+
+	elif request.method == 'Post':
+		data = JSONParser().parse(request)
+		serializer = ArticleSerializer(data=data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=201)
+		return JsonResponse(serializer.errors, status=400)
+	
