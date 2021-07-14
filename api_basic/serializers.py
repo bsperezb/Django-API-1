b69d8a7838b66a9f370cc8475c4ca4c85b81f676
brayan <brayanperezbuitrago@gmail.com>
2021-07-13 22:11:22 -0500

    REST Framework api_view() Decorator

--- api_basic/serializers.py
+++ api_basic/serializers.py
@@ -7,4 +7,6 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Article
-		fields = ['id', 'title', 'author']
+		#fields = ['id', 'title', 'author', 'email']
+		fields = '__all__'
+
