450039ab9740f5e849dae2af4780bb179b608c19
brayan <brayanperezbuitrago@gmail.com>
2021-07-18 00:12:22 -0500

    Viewsets and Routers

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from rest_framework import serializers
@@ -15,6 +16,41 @@ from rest_framework import generics	#vistas genéricas
 from rest_framework import mixins
 from rest_framework.authentication import SessionAuthentication, BaseAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404 
+
+class ArticleViewSet(viewsets.ViewSet):
+	def list(self, request):
+		articles = Article.objects.all()
+		serializer = ArticleSerializer(articles, many=True)
+		return Response(serializer.data )
+
+	def create(self, request):
+		serializer = ArticleSerializer(data=request.data)
+
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	def retrieve(self, request, pk=None):
+		queryset = Article.objects.all() 		
+		article = get_object_or_404(queryset, pk=pk)	
+		serializer = ArticleSerializer(article)
+		return Response(serializer.data)
+	
+	def update(self,request, pk=None):
+		article = Article.objects.get(pk=pk)
+		serializer = ArticleSerializer(article, data=request.data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)	
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	  
+
+
+
 
 class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin,
 	mixins.CreateModelMixin, mixins.UpdateModelMixin, 
